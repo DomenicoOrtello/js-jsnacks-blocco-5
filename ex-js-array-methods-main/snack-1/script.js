449e752083ada8326db5c01e9426367be7d983df
Ortello7 <domenicoortello00@gmail.com>
2024-12-17 12:33:34 +0100

    snack 1 fixed

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,4 +2,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
-console.log(names);
+// cicla prima di stampare
+names.forEach(function(name) {
+    console.log(name);
+    });
